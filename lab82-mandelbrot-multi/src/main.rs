ffe580c97f3867c30893874c8745741c96a4b96d
Tanapat Chamted <91345773+Makufff@users.noreply.github.com>
2025-11-24 02:27:17 +0700

    mandelbrot

--- lab82-mandelbrot-multi/src/main.rs
+++ lab82-mandelbrot-multi/src/main.rs
@@ -20,17 +20,44 @@ fn main() {
 
     // TODO: Calculate all pixels in parallel (based on lab 81-mandelbrot-single)
 
-
     // Placeholder for pixel calculations
     let pixels: Vec<(u32, u32, Rgb<u8>)> =
         (0..image_height)
-        .flat_map(|y| {
-            (0..image_width).map(move |x| {
-                let pixel: Rgb<u8> = Rgb([0, 0, 0]);
-                (x, y, pixel)
+            .into_par_iter()
+            .flat_map(|y| {
+                (0..image_width).into_par_iter().map(move |x| {
+
+                    // TODO: Optimize mapping from pixel to complex plane
+                    let cx = x_min + (x as f64 / image_width as f64) * (x_max - x_min);
+                    let cy = y_min + (y as f64 / image_height as f64) * (y_max - y_min);
+
+                    let c = Complex::new(cx, cy);
+                    let mut z = Complex::new(0.0, 0.0);
+
+                    let mut i = 0;
+
+                    while i < max_iterations && z.norm() <= 2.0 {
+                        z = z * z + c;
+                        i += 1;
+                    }
+
+                    let pixel = if i == max_iterations {
+
+                        Rgb([0, 0, 0])
+
+                    } else {
+
+                        let r = (i % 255) as u8 ;
+                        let g = (i * 5 % 255) as u8 ;
+                        let b = (i * 10 % 255) as u8 ;
+
+                        Rgb([r, g, b])
+                    };
+
+                    (x , y , pixel)
+                })
             })
-        })
-        .collect();
+            .collect();
     // Write pixels to image buffer
     for (x, y, pixel) in pixels {
         imgbuf.put_pixel(x, y, pixel);
